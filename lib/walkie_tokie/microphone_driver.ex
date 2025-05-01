230aadb611d9be059dfec9308c39f1b82ff89ff1
Kevin <kevinam99@gmail.com>
2025-05-01 11:18:33 +0530

    Improve error handling and update error message

--- lib/walkie_tokie/microphone_driver.ex
+++ lib/walkie_tokie/microphone_driver.ex
@@ -73,18 +73,18 @@ defmodule WalkieTokie.MicrophoneDriver do
   def handle_info(:start_talking, state) do
     updated_state = set_dict(state, :is_talking, true)
 
-    os =
+    {os, executable_name} =
       case :os.type() do
-        {:win32, _} -> "windows"
-        {:unix, :darwin} -> "mac"
-        {:unix, _} -> "linux"
+        {:win32, _} -> {"windows", "sox"}
+        {:unix, :darwin} -> {"mac", "sox"}
+        {:unix, _} -> {"linux", "arecord"}
       end
 
     {path, args} =
       case os do
         "mac" ->
           {
-            System.find_executable("sox"),
+            System.find_executable(executable_name),
             [
               "--buffer",
               "3200",
@@ -105,7 +105,7 @@ defmodule WalkieTokie.MicrophoneDriver do
 
         _linux_or_other ->
           {
-            System.find_executable("arecord"),
+            System.find_executable(executable_name),
             [
               "-r",
               "16000",
@@ -129,7 +129,7 @@ defmodule WalkieTokie.MicrophoneDriver do
 
     # Sanity check: Se path for nil, falha com log
     if path == nil do
-      Logger.error("Executável de captura de áudio não encontrado!")
+      Logger.error("Executável de captura de áudio não encontrado! Por favor, instale o #{executable_name} para usar este aplicativo.")
       {:noreply, set_dict(updated_state, :audio_port, nil)}
     else
       port = Port.open({:spawn_executable, path}, [:binary, :stream, args: args])
